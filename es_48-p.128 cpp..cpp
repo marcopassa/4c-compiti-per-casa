2ca91230b70891a977a73a5bcef6a2387629b251
marcopassa <marcopassa98@gmail.com>
2016-11-07 12:35:43 +0100

    Add files via upload

---
+++ es_48-p.128 cpp..cpp
@@ -0,0 +1,12 @@
+//es_48-p.128
+
+#include<iostream>
+using namespace std;
+
+
+enum Mesi{Gennaio=1, Febbraio, Marzo, Aprile, Maggio, Giugno, Luglio, Agosto, Settembre, Ottobre, Novembre, Dicembre};
+
+int main(){
+cout<<"I numeri dei mesi di vacanza sono"<<'\t'<< Giugno<<'\t' << Luglio << '\t'<< Agosto;
+return 0;
+}
---
+++ es_49-p.128.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+enum semaforo{VERDE, GIALLO, ROSSO};
+
+int main(){
+return 0;
+}
